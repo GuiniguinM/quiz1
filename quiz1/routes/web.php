5fc18383dc6bee68b60bc974f1a358fc544f5e62
GuiniguinM <146744399+GuiniguinM@users.noreply.github.com>
2025-03-18 00:46:25 +0000

    Creation of Quiz1

---
+++ quiz1/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventoryController;
+
+// This tells the website to listen for a link like "hello/John" and grab the "John" part
+
+Route::get('hello/{name}', function($name) {
+
+    return 'hi ' . $name; // It will say "hi" followed by whatever name you typed (like "hi John")
+
+});
+
+Route::resource('/inventory', InventoryController::class);
